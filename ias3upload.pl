20035fe15ed2697733e148367c2a23167f7a0bb2
Kenji Nagahashi <kenji@archive.org>
2013-04-25 11:47:26 -0700

    put GPLv3 notice, add LICENSE file

--- ias3upload.pl
+++ ias3upload.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/env perl
 #
+# ias3upload.pl - simple script for bulk-uploading to Internet Archive.
+# Copyright (C) 2013  Kenji Nagahashi <kenji@archive.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 use strict;
 use warnings;
 #use FindBin;
@@ -963,6 +979,7 @@ while (@uploadQueue) {
 		   $file->{mtime}, $file->{item}{name},
 		   uri_escape($file->{filename}))
 		if $file->{file};
+	    $jnl->flush();
 	    print "\n";
 	} else {
 	    print $res->status_line, "\n", $res->content, "\n\n";
